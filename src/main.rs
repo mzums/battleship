00210742b972aac6457bd540e109a35c20bb6170
mzums <annsie035@gmail.com>
2024-12-17 22:15:57 +0100

    placing ships

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 use std::io;
 use regex::Regex;
 
-fn print_board() {
+fn print_board(players_board: &[[i32; 10]; 10]) {
     println!("   A  B  C  D  E  F  G  H  I  J");
     for i in 0..10 {
         print!("{i} ");
-        for _ in 0..10 {
-            print!("|  ");
+        for j in 0..10 {
+            print!("| ");
+            print!("{}", players_board[i][j]);
         }
         println!("|");
     }
 }
 
-fn parse_position(position: &str) -> Option<(char, u8)> {
-    let re = Regex::new(r"^[a-zA-Z]\d$").unwrap();
+fn parse_position(position: &str) -> Option<(usize, usize)> {
+    let re = Regex::new(r"^[a-jA-J]\d$").unwrap();
     if re.is_match(position) {
-        let row = position.chars().nth(0).unwrap().to_lowercase().next().unwrap();
-        let col = position.chars().nth(1).unwrap().to_digit(10).unwrap() as u8;
+        let row = position.chars().nth(0).unwrap().to_lowercase().next().unwrap() as usize - 'a' as usize;
+        let col = position.chars().nth(1).unwrap().to_digit(10).unwrap() as usize;
         Some((row, col))
     } else {
         None
     }
 }
 
-fn input_ships_pos() -> ((char, u8), (char, u8)) {
+fn input_ships_pos() -> ((usize, usize), (usize, usize)) {
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("error: unable to read user input");
@@ -41,28 +42,55 @@ fn input_ships_pos() -> ((char, u8), (char, u8)) {
     }
 }
 
-fn place_ship(len: i8) {
-    println!("Where would you like to place your {} ship?", "X".repeat(len as usize));
-    println!("Pass chosen range e.g. A0 {}0", ('A' as u8 + ((len-1) as u8)) as char);
+fn place_ship(players_board: &mut [[i32; 10]; 10], len: usize) {
+    loop {
+        println!("Where would you like to place your {} ship?", "X".repeat(len));
+        println!("Pass chosen range e.g. A0 {}0", ('A' as u8 + ((len - 1) as u8)) as char);
 
-    let ((r1_row, r1_col), (r2_row, r2_col)) = input_ships_pos();
-    print!("{r1_row}");
+        let ((r1_row, r1_col), (r2_row, r2_col)) = input_ships_pos();
 
-    if (r1_row as u8 - r2_row as u8 == (len-1) as u8 && r1_col == r2_col) || (r1_col as u8 - r2_col as u8 == (len-1) as u8 && r1_row == r2_row) {
-        return;
+        if r1_row == r2_row && (r1_col as isize - r2_col as isize).abs() == (len - 1) as isize {
+            let start = r1_col.min(r2_col);
+            for i in 0..len {
+                players_board[start + i][r1_col] = 1;
+            }
+            println!("Ship placed horizontally.");
+            break;
+        } else if r1_col == r2_col && (r1_row as isize - r2_row as isize).abs() == (len - 1) as isize {
+            let start = r1_row.min(r2_row);
+            for i in 0..len {
+                players_board[r1_row][start + i] = 1;
+            }
+            println!("Ship placed vertically.");
+            break;
+        } else {
+            println!(
+                "Invalid placement. Ensure the ship is aligned and has a length of {}.",
+                len
+            );
+        }
     }
-    
 }
 
-fn place_users_ships() {
-    place_ship(5);
+
+fn place_users_ships(players_board: &mut [[i32; 10]; 10]) {
+    place_ship(players_board, 5);
+    print_board(&players_board);
+    place_ship(players_board, 4);
+    print_board(&players_board);
+    place_ship(players_board, 3);
+    print_board(&players_board);
+    place_ship(players_board, 3);
+    print_board(&players_board);
+    place_ship(players_board, 2);
+    print_board(&players_board);
 }
 
 fn main() {
     println!("Hello, world!");
-    let _players_board: [[i32; 10]; 10] = [[0; 10]; 10];
+    let mut players_board: [[i32; 10]; 10] = [[0; 10]; 10];
 
-    print_board();
+    print_board(&players_board);
     println!();
-    place_users_ships();
+    place_users_ships(&mut players_board);
 }
